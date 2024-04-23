fea637f9cd99df8d712c32e1a9573c13e938bc71
bauka <newnewernewest123@gmail.com>
2024-04-23 13:45:50 +0500

    deleted important html sending email fragments

--- MyCrud/urls.py
+++ MyCrud/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import create_new_task, flickr, upload,blog, send_email
+from .views import create_new_task, flickr, upload,blog
 urlpatterns = [
     path('', create_new_task, name='create_new_task'),
     path('flickr/', flickr, name='flickr'),
     path('upload/', upload, name = 'upload'),
     path('blog/', blog, name='blog'),
-    path('sendmail/', send_email, name='send_email')
+    # path('sendmail/', send_email, name='send_email'),
+    # path('send-email/', send_email_view, name='send_email'),
 ]
--- MyCrud/views.py
+++ MyCrud/views.py
@@ -1,10 +1,10 @@
 from django.http import JsonResponse
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse, HttpResponseRedirect
 
 from .forms import BlogPostForm, UploadForm
 from .models import BlogPost
 from .serializers import BlogSerializer
-from django.core.mail import send_mail
+from django.core.mail import send_mail, BadHeaderError
 from django.conf import settings
 import ssl
 # Create your views here.
@@ -57,16 +57,37 @@ def blog(request):
     serializer = BlogSerializer()
     return JsonResponse(serializer.data)
 
-def send_email(request):
-    if request.method=='POST':
-        message = request.POST['message']
-        email = request.POST['email']
-        name = request.POST['name']
-        send_mail(
-            'Contact_mail',
-            message,
-            'settings.EMAIL_HOST_USER',
-            [email, 'bauyrzanbakbergen87@gmail.com', 'newnewernewest123@gmail.com'],
-            fail_silently=False
-        )
-        return render(request, 'wg_early_bird_template/index.html')
+# def send_email(request):
+#     if request.method=='POST':
+#         message = request.POST['message']
+#         email = request.POST['email']
+#         name = request.POST['name']
+#         send_mail(
+#             'Contact_mail',
+#             message,
+#             'settings.EMAIL_HOST_USER',
+#             [email],
+#             fail_silently=False
+#         )
+#         return render(request, 'wg_early_bird_template/index.html')
+
+# send_mail(
+#     "Subject here",
+#     "Here is the message.",
+#     "baukabakbergen003@gmail.com",
+#     ["bauyrzanbakbergen87@gmail.com"],
+#     fail_silently=False,
+# )
+#bauyrzanbakbergen87@gmail.com
+#'bauyrzanbakbergen87@gmail.com', 'newnewernewest123@gmail.com'
+# def send_email_view(request):
+#     subject = 'Hello from MoneyApp'
+#     message = 'You are succefully authenticated!'
+#     sender = 'baukabakbergen003@gmail.com'
+#     recipient_list = ['bauyrzanbakbergen87@gmail.com', 'newnewernewest123@gmail.com']
+
+#     try:
+#         send_mail(subject, message, sender, recipient_list)
+#         return HttpResponse("Email sent successfully")
+#     except Exception as e:
+#         return HttpResponse(f"An error occurred: {str(e)}")
